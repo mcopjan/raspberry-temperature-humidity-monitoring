e075a00455ea4fb10aec03a1517a58ad21111283
Martin Copjan <martin.copjan@controlup.com>
2023-07-26 15:31:01 +0100

    wip

--- RaspberryTemperatureHumidityApi/Model/agent.py
+++ RaspberryTemperatureHumidityApi/Model/agent.py
@@ -3,22 +3,32 @@ import sys
 import Adafruit_DHT
 import time
 
-url = 'http://{}/roomstats'
+url = sys.argv[1]
+sensor = sys.argv[2]
+pin = sys.argv[3]
+roomName = sys.argv[4]
+intervalInSec = sys.argv[5]
+
+  #sudo python3 send_post.py 192.168.5.199:5144 11 17 homeOffice 10
 
 while True:
     try:
-        humidity, temperature = Adafruit_DHT.read_retry(11, 17)
+        humidity, temperature = Adafruit_DHT.read_retry(int(11),int(17))
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         myobj = {
-                  "roomName": "pythonTest",
+                  "roomName": roomName,
                   "temperature": temperature,
                   "humidity": humidity,
                   "temperatureUnit": 1
                 }
-        x = requests.post(url, json = myobj)
+        x = requests.post('http://{0}/roomstats'.format(url), json = myobj)
         print(x.text)
     except RuntimeError as e:
-        # Reading doesn't always work! Just print error and we'll try again
         print("Reading from DHT failure: ", e.args)
 
-    time.sleep(10)
+    time.sleep(int(intervalInSec))
+
+
+
+
+
