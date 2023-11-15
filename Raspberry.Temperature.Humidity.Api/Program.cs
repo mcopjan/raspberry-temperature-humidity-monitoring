bc69f355f60672cb0f515d7bdc75f6ba7d62f9ce
Martin Copjan <martincopjan@gmail.com>
2023-11-15 21:02:24 +0000

    Store historical data

--- Raspberry.Temperature.Humidity.Api/Program.cs
+++ Raspberry.Temperature.Humidity.Api/Program.cs
@@ -1,63 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
+using Raspberry.Temperature.Humidity.Api.Repository;
 using RaspberryTemperatureHumidityApi.Model;
 using System.Collections.Concurrent;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        ConcurrentBag<string> RoomNames = new ConcurrentBag<string>();
+
+
+        app.MapGet("/roomstats/{roomName}", (string roomName) =>
+        {
+            var roomStats = FileRepository.ReadRoomsStatsFromFile();
+            return roomStats.Where(r => r.RoomName.Equals(roomName)).ToList();
+        });
+
+        app.MapGet("/roomnames", () =>
+        {
+            return RoomNames.ToList();
+        });
+
+        app.MapPost("/roomstats", ([FromBody] RoomStats stats) =>
+        {
+            try
+            {
+                if (RoomNames.All(r => !r.Equals(stats.RoomName)))
+                    RoomNames.Add(stats.RoomName);
+
+                stats.CreatedAt = DateTime.Now;
+                FileRepository.WriteRoomStatsIntoFile(stats);
+                Results.Ok(stats);
+            }
+            catch (Exception)
+            {
+                Results.Problem("A problem has occured while storing rooms statistics");
+            }
+        });
+
+        app.MapGet("/hello", async context =>
+        {
+            context.Response.StatusCode = 200;
+            await context.Response.WriteAsync("Api is up and running.");
+        });
+
+        app.Run();
+    }
 }
-
-ConcurrentBag<RoomStats> RoomStatistics = new ConcurrentBag<RoomStats>();
-ConcurrentBag<string> RoomNames = new ConcurrentBag<string>();
-
-
-app.MapGet("/roomstats/{roomName}", (string roomName) =>
-{
-    return RoomStatistics.Where(r=>r.RoomName.Equals(roomName)).ToList();
-})
-.WithName("GetRoomStats");
-
-app.MapGet("/roomnames", () =>
-{
-    return RoomNames.ToList();
-    //return new List<string>() { "room1","room2" };
-})
-.WithName("GetRoomNames");
-
-app.MapPost("/roomstats", async ([FromBody] RoomStats stats) =>
-{
-    try
-    {
-        if (RoomNames.All(r => !r.Equals(stats.RoomName)))
-            RoomNames.Add(stats.RoomName);  
-
-        stats.CreatedAt = DateTime.Now;
-        await Task.Run(() => RoomStatistics.Add(stats));
-        Results.Ok(stats);  
-    }
-    catch (Exception)
-    {
-        Results.Problem("Error creating new employee record");
-    }
-})
-.WithName("PostRoomStats");
-
-app.MapGet("/hello", async context =>
-{
-    context.Response.StatusCode = 200;
-    await context.Response.WriteAsync("Api is up and running.");
-})
-.WithName("Hello");
-
-app.Run();
---
+++ Raspberry.Temperature.Humidity.Api/Repository/FileRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using RaspberryTemperatureHumidityApi.Model;
+
+namespace Raspberry.Temperature.Humidity.Api.Repository
+{
+    public static class FileRepository
+    {
+        static string filePath = "rooms-stats.json";
+        static object fileLock = new object();
+
+        static FileRepository()
+        {
+            if(!File.Exists(filePath))
+                File.Create(filePath);
+        }
+        
+
+        public static List<RoomStats> ReadRoomsStatsFromFile()
+        {
+            lock (fileLock)
+            {
+                string jsonContent = File.ReadAllText(filePath);
+
+                if (!string.IsNullOrEmpty(jsonContent))
+                {
+                    List<RoomStats> stats = JsonSerializer.Deserialize<List<RoomStats>>(jsonContent);
+                    return stats;
+                }
+                return null;
+            }
+            
+        }
+
+        public static void WriteRoomStatsIntoFile(RoomStats data)
+        {
+            lock (fileLock)
+            {
+                List<RoomStats> stats;
+
+                string jsonContent = File.ReadAllText(filePath);
+                stats = !string.IsNullOrEmpty(jsonContent) ? JsonSerializer.Deserialize<List<RoomStats>>(jsonContent) : new List<RoomStats>();
+                stats.Add(data);
+                string updatedJsonContent = JsonSerializer.Serialize(stats);
+                File.WriteAllText(filePath, updatedJsonContent);
+            }
+            
+        }
+    }
+}
