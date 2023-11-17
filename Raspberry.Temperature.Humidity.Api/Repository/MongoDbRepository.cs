b3f0aecf225c8cd57e37ca7f0656c28846902e90
Martin Copjan <martincopjan@gmail.com>
2023-11-17 21:28:33 +0000

    add mongo host and port as env vars

--- Raspberry.Temperature.Humidity.Api/Repository/MongoDbRepository.cs
+++ Raspberry.Temperature.Humidity.Api/Repository/MongoDbRepository.cs
@@ -7,13 +7,17 @@ namespace Raspberry.Temperature.Humidity.Api.Repository
 {
     public static class MongoDbRepository
     {
-        private static string connectionString = "mongodb://192.168.5.208:27017";
+        private static string connectionString;
         private static string databaseName = "RoomStatsDatabase";
         private static readonly MongoClient _client;
 
         static MongoDbRepository()
         {
             _client = new MongoClient(connectionString);
+            string mongoHost = Environment.GetEnvironmentVariable("MONGO_HOST") ?? "localhost";
+            string mongoPortStr = Environment.GetEnvironmentVariable("MONGO_PORT") ?? "27017";
+            Console.WriteLine($"Mongo host set to {mongoHost} and mongo port to {mongoPortStr}");
+            connectionString = $"mongodb://{mongoHost}:{mongoPortStr}";
         }
 
 
@@ -31,19 +35,6 @@ namespace Raspberry.Temperature.Humidity.Api.Repository
             var database = _client.GetDatabase(databaseName);
             var collection = database.GetCollection<BsonDocument>(roomName);
             var documents = await collection.Find("{}").ToListAsync();
-
-            if (documents[0].Contains("_id"))
-            {
-                // Access the value of the _id field
-                BsonValue idValue = documents[0]["_id"];
-
-                // Check if the value is a valid ObjectId
-                if (idValue.IsObjectId)
-                {
-                    string idString = idValue.AsObjectId.ToString();
-                    Console.WriteLine($"Found valid ObjectId: {idString}");
-                }
-            }
 
             List<RoomStats> allRoomStats = documents
             .Select(bsonDocument => BsonSerializer.Deserialize<RoomStats>(bsonDocument))
